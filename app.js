891d9617028b9fe9260f4f93f1284118fdaf16c3
davidbt07 <david.ballesteros@udea.edu.co>
2021-08-14 10:30:03 -0500

    third

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const express = require('express');
-const router = express.Router();
-const app = express();
+const express = require('express')
+const router = express.Router()
+const app = express()
 
-router.get('/', (req, res)=> {
-    return res.status(200).send({
-        message: 'Hello'
-    })
+
+router.get('/', (req,res) =>{
+    return res.status(200).send('HELLO')
 })
 
 app.use(router)
 
 app.listen(5000, ()=>{
-    console.log('Server running on port 5000')
+    console.log('Servidor corriendo en el puerto 5000')
 })
